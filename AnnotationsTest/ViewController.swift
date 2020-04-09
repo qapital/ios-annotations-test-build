ab19c06ec687562d9ead0e431e1f45fd6f5afdb7
Mikael Gransell <gransell@gmail.com>
2020-04-09 14:44:56 +0200

    First commit

---
+++ AnnotationsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnnotationsTest
+//
+//  Created by Mikael Gransell on 2020-04-09.
+//  Copyright © 2020 Qapital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
